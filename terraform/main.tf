1c44ec398d915925608ac16bfc4b88083ead17dc
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-09-25 20:42:30 +0100

    add terraform code for creating ec2 instance and s3 bucket

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "oneinstance" {
+    ami = "ami-087c17d1fe0178315"
+    instance_type = "t2.micro"
+    key_name = "flugeladmin"
+
+    tags = {
+        Name = "Flugel"
+        Owner = "InfraTeam"
+    }
+}
+
+resource "aws_s3_bucket" "onebucket" {
+    bucket = "buc955815154140"
+    acl = "public-read"
+
+    versioning {
+      enabled = true
+   }
+
+    tags = {
+        Name = "Flugel"
+        Owner = "InfraTeam"
+    }
+}
