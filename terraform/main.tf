ce57a8cd111b07b2e2cc01aea3ad3d70ca9cc9ea
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-09-26 14:04:00 +0100

    move .tf files to terraform folder

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "mariam-demo"
+    organization = "flugel-challenge"
 
     workspaces {
-      name = "mariam-demo"
+      name = "flugel-challenge"
     }
   }
 }
