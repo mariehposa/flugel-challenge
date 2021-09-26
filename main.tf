b530b871c9a23b49d3e81c24402a3dc6fc84ca6f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-09-26 12:20:14 +0100

    .

--- main.tf
+++ main.tf
@@ -21,30 +21,30 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "oneinstance" {
-    ami = "ami-087c17d1fe0178315"
-    instance_type = "t2.micro"
-    key_name = "flugeladmin"
+  ami = "ami-087c17d1fe0178315"
+  instance_type = "t2.micro"
+  key_name = "flugeladmin"
 
-    tags = {
-        Name = "Flugel"
-        Owner = "InfraTeam"
-    }
+  tags = {
+    Name = "Flugel"
+    Owner = "InfraTeam"
+  }
 }
 
 resource "aws_s3_bucket" "onebucket" {
-    bucket = "buc955815154140"
-    acl = "public-read"
+  bucket = "buc955815154140"
+  acl = "public-read"
 
-    versioning {
-      enabled = true
-   }
+  versioning {
+    enabled = true
+  }
 
-    tags = {
-        Name = "Flugel"
-        Owner = "InfraTeam"
-    }
+  tags = {
+    Name = "Flugel"
+    Owner = "InfraTeam"
+  }
 }
