47425a33f9259e216417427a70a54821a85c13d7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-09-26 12:26:02 +0100

    .

--- main.tf
+++ main.tf
@@ -25,26 +25,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "oneinstance" {
-  ami = "ami-087c17d1fe0178315"
+  ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-  key_name = "flugeladmin"
+  key_name      = "flugeladmin"
 
   tags = {
-    Name = "Flugel"
+    Name  = "Flugel"
     Owner = "InfraTeam"
   }
 }
 
 resource "aws_s3_bucket" "onebucket" {
   bucket = "buc955815154140"
-  acl = "public-read"
+  acl    = "public-read"
 
   versioning {
     enabled = true
   }
 
   tags = {
-    Name = "Flugel"
+    Name  = "Flugel"
     Owner = "InfraTeam"
   }
 }
